abe23582cb766df58631f321e2c47978ef8de64b
jouhs <jaouad.ouhs@gmail.com>
2016-12-11 14:31:31 +0100

    add sum

--- BonjourTest/src/org/app/selfservice/Bonjour.java
+++ BonjourTest/src/org/app/selfservice/Bonjour.java
@@ -1,11 +1,18 @@
 package org.app.selfservice;
 
+
 public class Bonjour {
 
 	public static void main(String[] args) {
 		//affichage message
 		System.out.println("Bonjour tout le monde");
-
+		int a=10, b=15;
+		System.out.println(a+" + "+b+" = "+getSumAB(a, b));
+		
+	}
+	//ajout fct sum
+	public static int getSumAB(int a, int b){
+		return a+b;
 	}
 
 }
